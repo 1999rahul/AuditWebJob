5551c1832a5bb99c50a81c59e1c31418dcb3a45b
Rahul Kumar <1999rahul.py@gmail.com>
2023-02-13 20:21:53 +0530

    completed

---
+++ QueueProcessor/Program.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Queues.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nancy.Json;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace QueueProcessor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorageQueues();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+        public static void ProcessQueueMessage([QueueTrigger("audit-queue")] string message, ILogger logger)
+        {
+            var fact= new CookBookContextFactory().CreateDbContext();
+            fact.data.Add(new AuditEmployeeData() {Data= message });
+            fact.SaveChanges();
+        }
+    }
+}
---
+++ QueueProcessor/QueueMessege.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueProcessor
+{
+    [Table("tblAuditEmployeeData")]
+    public class AuditEmployeeData
+    {
+        public int? ID { get; set; }
+        
+        public string? Data { get; set; }    
+    }
+
+    public class AuditEmployeeDataContext : DbContext
+    {
+        public DbSet<AuditEmployeeData> data { get; set; }
+        public AuditEmployeeDataContext(DbContextOptions<AuditEmployeeDataContext> options) : base(options)
+        {
+
+        }
+
+    }
+    public class CookBookContextFactory : IDesignTimeDbContextFactory<AuditEmployeeDataContext>
+    {
+        public AuditEmployeeDataContext CreateDbContext(string[]? args = null)
+        {
+            /* var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();*/
+            var optionsBuilder = new DbContextOptionsBuilder<AuditEmployeeDataContext>();
+            optionsBuilder.UseSqlServer("Server=tcp:my-demo-server.database.windows.net,1433;Initial Catalog=EmployeeDB;Persist Security Info=False;User ID=demo-sql-database-server;Password=Rahul1234@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            return new AuditEmployeeDataContext(optionsBuilder.Options);
+        }
+    }
+}
